50cfceeb2c82a6b6d3f800466870ead39181ad8c
Thomas Leu <tomleu@gmail.com>
2016-01-27 10:05:45 -0600

    Update post slugging

--- app/Post.php
+++ app/Post.php
@@ -14,23 +14,32 @@ class Post extends Model implements SluggableInterface
     use SluggableTrait;
 
     /**
-     * Attribtues that are mass assignable.
+     * Attributes that are mass assignable.
      *
      * @var array
      */
-    protected $fillable = ['title', 'content', 'published_at'];
+    protected $fillable = ['user_id', 'title', 'content', 'published_at'];
 
     /**
-     * Configuration variables for slugging
+     * Configuration variables for slugging.
      *
      * @var array
      */
-    protected $sluggable = ['build_from' => 'title'];
+    protected $sluggable = [
+        'build_from' => 'title',
+        'on_update' => true
+    ];
 
+    /**
+     * The tags that should not be stripped from a post's content.
+     *
+     * @var string
+     */
     protected $allowableTags = "<pre><code><p>";
 
     /**
      * Get the article's html
+     *
      * @return string
      */
     public function getHtmlAttribute()
@@ -40,6 +49,7 @@ class Post extends Model implements SluggableInterface
 
     /**
      * Get a summary of the article.
+     *
      * @return string
      */
     public function getSummaryAttribute()
@@ -48,6 +58,8 @@ class Post extends Model implements SluggableInterface
     }
 
     /**
+     * Set the publish date for a post.
+     *
      * @param mixed $date
      */
     public function setPublishedAtAttribute($date)
@@ -56,6 +68,8 @@ class Post extends Model implements SluggableInterface
     }
 
     /**
+     * Get the publish date for a post.
+     *
      * @return string
      */
     public function getPublishedAtAttribute()
@@ -65,7 +79,7 @@ class Post extends Model implements SluggableInterface
     }
 
     /**
-     * Get the published articles
+     * Get all articles published on or prior to the current time.
      *
      * @param Builder $query
      * @return Builder
@@ -75,10 +89,12 @@ class Post extends Model implements SluggableInterface
         return $query->where('published_at', '<=', \Carbon\Carbon::now());
     }
 
+
     /**
+     * Get $count most recent articles.
+     *
      * @param Builder $query
      * @param int     $count
-     *
      * @return Builder $query
      */
     public function scopeRecent(Builder $query, int $count)
@@ -98,6 +114,7 @@ class Post extends Model implements SluggableInterface
 
     /**
      * An article belongs to one user
+     *
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
      */
     public function user()
@@ -107,6 +124,7 @@ class Post extends Model implements SluggableInterface
 
     /**
      * Get all tags associated with the post
+     *
      * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
      */
     public function tags()
