619697a1cf49e32ac5d8ec13f32fcd3900968fb0
Thomas Leu <tomleu@gmail.com>
2016-01-29 18:17:14 -0600

    Add UI for searching and error states

--- resources/views/code/jwsearch.blade.php
+++ resources/views/code/jwsearch.blade.php
@@ -7,6 +7,7 @@
 @section('content')
 
    <div id="app">
+       {{-- JWPlayer Component --}}
        <video-player
            url="https://www.youtube.com/watch?v=ZTLAx3VDX7g"
            title="Star Wars Acapella"
@@ -14,22 +15,41 @@
                     theme songs from Star Wars"
        ></video-player>
 
+       {{-- Youtube Search Box Component --}}
        <video-search></video-search>
 
-
+       {{-- JWPlayer Component Template --}}
        <template id="video-player-template">
-           <div id="video"><i class="fa fa-spinner fa-spin"></i> Loading video player...</div>
+           <div id="video">
+               <i class="fa fa-spinner fa-spin"></i> Loading video player...
+           </div>
        </template>
 
+       {{-- Youtube Search Box Component Template --}}
        <template id="video-search-template">
            <div class="search-results">
                <div class="input-group">
-                   <input type="text" v-model="query" class="form-control" placeholder="Search Youtube...">
-              <span class="input-group-btn">
-                  <button class="btn btn-default" @click="search">Search</button>
-              </span>
+                  <input type="text"
+                         v-model="query"
+                         @keyup.enter="search"
+                         class="form-control"
+                         placeholder="Search Youtube...">
+                  <span class="input-group-btn">
+                      <button class="btn btn-default" @click="search">Search</button>
+                  </span>
                </div>
-               <ul class="list-unstyled">
+               {{-- Searching Spinner --}}
+               <div class="alert alert-info" v-if="searching">
+                   <i class="fa fa-spinner fa-spin"></i> Searching...
+               </div>
+
+               {{-- Error message --}}
+               <div class="alert alert-warning" v-if="!searching && !!errorMessage">
+                   @{{ errorMessage }}
+               </div>
+
+               {{-- Search Results --}}
+               <ul v-if="!searching && !errorMessage" class="list-unstyled">
                    <li v-for="result in results" class="result">
                        <div @click="setVideo($index)">
                            <div class="video-thumbnail">
@@ -49,9 +69,12 @@
 @stop
 
 @section('script')
+    {{-- Import JWPlayer and set key  --}}
     <script src="/js/lib/jwplayer.js"></script>
     <script>jwplayer.key="N9BKmmi9T17VvXMGliiUJKAlBEcW2qk9He72pw==";</script>
+    {{-- Import Vue.js --}}
     <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.15/vue.js"></script>
+    {{-- Vue components --}}
     <script>
         Vue.component('video-player', {
             template: '#video-player-template',
@@ -94,13 +117,15 @@
             template: '#video-search-template',
             data: function() {
                 return {
+                    searching: false,
                     query: "",
-                    results: []
+                    results: [],
+                    errorMessage: null
                 };
             },
             methods: {
                 search: function() {
-                    console.log('search');
+                    this.searching = true;
                     var self = this;
                     $.ajax({
                         data: { query: self.query },
@@ -108,6 +133,13 @@
                         success: function(data) {
                             var parser = new YoutubeSearchResultParser(data);
                             self.results = parser.parseSearchResults();
+                            self.errorMessage = null;
+                            self.searching = false;
+                        },
+                        error: function(err) {
+                            self.results = [];
+                            self.errorMessage = 'Whoops, something went wrong';
+                            self.searching = false;
                         }
                     });
                 },
